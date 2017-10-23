00c7934c3f59f2e0d87a3d18c05ac672be316a90
pbortnik <pavel_bortnik@epam.com>
2017-10-23 23:32:10 +0300

    initial commit

---
+++ src/main/java/by/pbortnik/analyzer/AnalyzerApplication.java
@@ -0,0 +1,12 @@
+package by.pbortnik.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzerApplication.class, args);
+	}
+}
