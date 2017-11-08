fd61aa54228b2cbe2a66b42a2a8189b6512c34b5
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-11-08 19:41:53 +0300

    adopt to new analyzer interface

--- src/main/java/by/pbortnik/analyzer/controller/AnalyzerController.java
+++ src/main/java/by/pbortnik/analyzer/controller/AnalyzerController.java
@@ -1,10 +1,12 @@
 package by.pbortnik.analyzer.controller;
 
+import by.pbortnik.analyzer.model.AnalyzedItemRs;
 import by.pbortnik.analyzer.model.IndexLaunch;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -15,15 +17,18 @@ public class AnalyzerController {
 	@RequestMapping(value = "/_analyze", method = RequestMethod.POST, consumes = APPLICATION_JSON)
 	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
-	public IndexLaunch[] analyze(@RequestBody List<IndexLaunch> launches) {
+	public List<AnalyzedItemRs> analyze(@RequestBody List<IndexLaunch> launches) {
+		List<AnalyzedItemRs> response = new ArrayList<>();
 		IndexLaunch indexLaunch = launches.get(0);
 		indexLaunch.getTestItems().forEach(item -> {
 			if (item.getLogs().stream().anyMatch(it -> it.getMessage().contains("AssertionError"))) {
 				item.setIssueType("PB001");
+				AnalyzedItemRs analyzedItem = new AnalyzedItemRs();
+				analyzedItem.setItemId(item.getTestItemId());
+				analyzedItem.setIssueType("PB001");
+				response.add(analyzedItem);
 			}
 		});
-		IndexLaunch[] indexLaunches = new IndexLaunch[1];
-		indexLaunches[0] = indexLaunch;
-		return indexLaunches;
+		return response;
 	}
 }
